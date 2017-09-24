dcfa5d1fd74962ee25d7e16ea1fb78eba6e96d0c
Rafael Sinosaki <rafaelsinosak@gmail.com>
2017-09-24 18:40:14 -0300

    Add files via upload

---
+++ API SMS Vivo.py
@@ -0,0 +1,13 @@
+import twilio.rest
+account_sid ="[seu id]"
+auth_token ="[seu token"
+
+
+client = twilio.rest.TwilioRestClient(account_sid, auth_token)
+
+message = client.messages.create(
+        body="".join(["rafaelsinosak\n" for i in range (10)]),
+        to="+[seu numero]",
+        from_="+16179257785"
+    )
+
